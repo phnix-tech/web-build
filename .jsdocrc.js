1c4f139dac31a57c03fa5051564aee6f21945ccb
leejefy <050934@gmail.com>
2020-10-03 21:57:09 +0800

    version 0.2.2

--- .jsdocrc.js
+++ .jsdocrc.js
@@ -1,8 +1,8 @@
 let cfg = require("@grmn/doc/.jsdocrc");
 cfg = JSON.parse(JSON.stringify(cfg));
 cfg.source.include = [
-    "fe/Functions.js",
-    "fe/Logging.js"
+  "fe/Functions.js",
+  "fe/Logging.js"
 ];
 
 module.exports = cfg;
