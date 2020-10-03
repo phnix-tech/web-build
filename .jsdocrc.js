8092489dec96c06a66f0d5227141fde8e2eec978
leejefy <050934@gmail.com>
2020-10-03 20:52:46 +0800

    修改基本信息 - 包名/地址/邮箱等

--- .jsdocrc.js
+++ .jsdocrc.js
@@ -2,11 +2,7 @@ let cfg = require("@grmn/doc/.jsdocrc");
 cfg = JSON.parse(JSON.stringify(cfg));
 cfg.source.include = [
     "fe/Functions.js",
-    "fe/Logging.js",
-    "fe/model/AppVariable.js",
-    "fe/service/Service.js",
-    "fe/GSMJsBridge.js",
-    "fe/common/util.js"
+    "fe/Logging.js"
 ];
 
 module.exports = cfg;
