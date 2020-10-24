87845c7debe9a006928cf16c8f508d790a5aaa46
leejefy <050934@gmail.com>
2020-10-24 23:43:50 +0800

    migrate build scripts from web common

---
+++ scripts/react/eslintrc.js
@@ -0,0 +1,9 @@
+/**
+ * eslint默认规则重写，或者添加自定义规则
+ * @param {*} cfg - eslint configuration object
+ * @returns {*} object
+ */
+module.exports = function (cfg) {
+  // react项目模板暂无规则重写
+  return cfg;
+};
---
+++ scripts/vue/eslintrc.js
@@ -0,0 +1,13 @@
+/**
+ * eslint默认规则重写，或者添加自定义规则
+ * @param {*} cfg - eslint configuration object
+ * @returns {*} object
+ */
+module.exports = function (cfg) {
+  cfg.rules = Object.assign(cfg.rules || {}, {
+    // 该规则不能正确解析export default from语法
+    "object-curly-spacing": "off"
+  });
+
+  return cfg;
+};
